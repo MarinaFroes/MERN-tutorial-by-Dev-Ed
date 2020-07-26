3e8bd5a6ff5adeb0388f44e1a934ff758d7cb95b
MFAC <ninafroes@gmail.com>
2020-07-26 21:16:04 +0200

    feat: Add Post model

---
+++ models/Post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// Schema - how the data looks
+const PostSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+module.exports = mongoose.model('Posts', PostSchema);
