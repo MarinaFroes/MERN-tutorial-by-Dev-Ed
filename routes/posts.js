d494d8c2425b9a54340285440e91a022d667680d
MFAC <ninafroes@gmail.com>
2020-07-26 21:39:40 +0200

    feat: Create new post

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
   res.send('We are on posts');
 });
 
-router.get('/specific', (req, res) => {
-  res.send('Specific post');
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  try {
+    const savedPost = await post.save();
+    res.json(savedPost);
+  } catch (e) {
+    res.json({ message: err });
+  }
 });
 
 module.exports = router;
